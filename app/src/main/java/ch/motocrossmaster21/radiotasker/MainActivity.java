9396983b5c72cdee13e28e9f3a6e83a1e63c80d3
motocrossmaster21 <38261775+motocrossmaster21@users.noreply.github.com>
2025-07-19 20:24:36 +0200

    Fix missing Intent import

--- app/src/main/java/ch/motocrossmaster21/radiotasker/MainActivity.java
+++ app/src/main/java/ch/motocrossmaster21/radiotasker/MainActivity.java
@@ -1,6 +1,7 @@
 package ch.motocrossmaster21.radiotasker;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
